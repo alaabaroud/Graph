714aad08eff0b8187d28df1970142e86e53818c9
ALAABAROUD <alaabaroud783@gmail.com>
2021-12-04 19:43:32 +0200

    trip

---
+++ app/src/main/java/Graph/Graph.java
@@ -0,0 +1,143 @@
+package Graph;
+import java.util.*;
+
+public class Graph {
+
+  private final Map<Vertex, List<Vertex>> adjVertices;
+
+  public Graph() {
+    adjVertices = new HashMap<>();
+  }
+
+  void addVertex(String data) {
+    Vertex vertex = new Vertex(data);
+    adjVertices.putIfAbsent(vertex, new ArrayList<>());
+  }
+
+  void addEdge(String data1, String data2) {
+    Vertex vertex1 = new Vertex(data1);
+    Vertex vertex2 = new Vertex(data2);
+
+    adjVertices.get(vertex1).add(vertex2);
+    adjVertices.get(vertex2).add(vertex1);
+  }
+
+
+  String printGraph() {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (Vertex vertex : adjVertices.keySet()) {
+      stringBuilder.append(vertex);
+      stringBuilder.append(adjVertices.get(vertex));
+    }
+
+    return stringBuilder.toString();
+  }
+
+  int size() {
+    return adjVertices.size();
+  }
+
+  public Set<Vertex> getVertices(){
+    return adjVertices.keySet();
+  }
+
+  Set<String> depthFirstTraverse(Graph graph, String root) {
+    Set<String> visited = new LinkedHashSet<>();
+    Stack<String> stack = new Stack<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      String vertex = stack.pop();
+      if (!visited.contains(vertex)) {
+        visited.add(vertex);
+
+        for (Vertex v : graph.getNeighbors(vertex)) {
+          stack.push(v.data);
+        }
+      }
+    }
+
+    return visited;
+  }
+
+
+  public List<Vertex> getNeighbors(String data) {
+    return adjVertices.get(new Vertex(data));
+  }
+
+//    <<< Code Challenge 36 >>>
+
+  Set<String> breadthTraverse( String root) {
+    Set<String> visited = new LinkedHashSet<>();
+    Queue<String> queue = new LinkedList<>();
+    queue.add(root);
+    visited.add(root);
+    while (!queue.isEmpty()) {
+      String vertex = queue.poll();
+      for (Vertex v : getNeighbors(vertex)) {
+        if (!visited.contains(v.data)) {
+          visited.add(v.data);
+          queue.add(v.data);
+        }
+      }
+    }
+    return visited;
+  }
+
+//    <<< Code Challenge Code 37 >>>
+
+  public void addEdgeWithWeight(String data1, String data2, int weight) {
+    Vertex Vertex1 = new Vertex(data1, weight);
+    Vertex Vertex2 = new Vertex(data2, weight);
+
+
+    adjVertices.get(Vertex1).add(Vertex2);
+    adjVertices.get(Vertex2).add(Vertex1);
+  }
+
+  int Cost;
+  public Integer businessTrip(Graph graph, List<String> cities){
+    Cost = 0;
+    int citiesSize = cities.size();
+
+    for (int i = 0; i < citiesSize - 1; i ++){
+
+      findPath(cities.get(i), cities.get(i + 1), graph);
+    }
+
+    return Cost;
+  }
+
+  private void findPath(String city1, String city2, Graph graph){
+
+    if (graph.getNeighbors(city1) == null){
+      return;
+    }
+    for (Vertex vertex: graph.getNeighbors(city1)) {
+      if (Objects.equals(city2, vertex.data)){
+       Cost += vertex.weight;
+      }
+    }
+  }
+
+
+  Set<String> depthFirstTraverse(String root) {
+    Set<String> visited = new LinkedHashSet<>();
+    Stack<String> stack = new Stack<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      String vertex = stack.pop();
+      if (!visited.contains(vertex)) {
+        visited.add(vertex);
+
+        for (Vertex v : getNeighbors(vertex)) {
+          stack.push(v.data);
+        }
+      }
+    }
+
+    return visited;
+  }
+
+}
---
+++ app/src/main/java/Graph/README.md
@@ -0,0 +1,7 @@
+# Graphs
+A Graph is a non-linear data structure consisting of nodes and edges. The nodes are sometimes also referred to as vertices and the edges are lines or arcs that connect any two nodes in the graph.
+## Challenge whiteBoard:
+
+- cc 36 code challenge:
+![trip](trip.png)
+
